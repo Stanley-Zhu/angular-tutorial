8ee9a1b442fb8262de493e8f0d18aef09a330285
Stanley Zhu <szhu@mtb.com>
2024-12-11 11:07:09 -0500

    init commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import {Component, inject} from '@angular/core';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from "../housinglocation";
+import {CommonModule} from "@angular/common";
+import {HousingService} from '../housing.service';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, HousingLocationComponent, FormsModule],
+  template: `
+    <section>
+      <form (ngSubmit)="filterResults(filter.value)">
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">
+          Search
+        </button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location 
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations()
+    // filtered list should be all housingLocations by default
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    } else {
+      this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+          housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+      );
+    }
+  }
+}
